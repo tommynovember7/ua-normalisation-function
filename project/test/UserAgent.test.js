f735eb15ab31166f9a52f59907acbe9c2c158671
Tomo L. Miyamoto <lancelot@tommyheavenly7.com>
2021-05-12 21:41:55 +0000

    feat: add default UA label if User-Agent header doesn't exist

--- project/test/UserAgent.test.js
+++ project/test/UserAgent.test.js
@@ -1,15 +1,24 @@
 const userAgents = require('./_fixture/userAgents.json');
 const userAgent = require('../src/UserAgent');
 
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max);
-};
+const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 describe('Mobile UA Detection Tests', () => {
   test('Find Safari', () => {
-    const UAString = userAgents.mobile[0][1];
-    expect(userAgent.isMobileViewer(UAString)).toBeTruthy();
-    expect(userAgent.getLabel(UAString)).toEqual('iPhone');
+    const UAString = [];
+    UAString.push(userAgents.mobile[0][1]);
+    UAString.push(userAgents.mobile[18][1]);
+    UAString.push(userAgents.mobile[19][1]);
+    UAString.push(userAgents.mobile[20][1]);
+    UAString.push(userAgents.mobile[21][1]);
+
+    UAString.forEach((item) => {
+      if (!userAgent.isMobileViewer(item)) {
+        console.log(item);
+      }
+      expect(userAgent.isMobileViewer(item)).toBeTruthy();
+      expect(userAgent.getLabel(item)).toEqual('iPhone');
+    });
   });
   test('Find Chrome Mobile', () => {
     const UAString = userAgents.mobile[2][1];
@@ -34,5 +43,9 @@ describe('Mobile UA Detection Tests', () => {
     let result = false;
     result = userAgents.others.every((UA) => userAgent.isMobileViewer(UA[1]));
     expect(result).toBeFalsy();
+  });
+  test('Missing User-Agent header', () => {
+    const result = userAgent.isMobileViewer('');
+    expect(result).toBeTruthy();
   });
 });
